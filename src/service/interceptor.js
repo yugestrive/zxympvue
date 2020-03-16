89453f373fa370821c2a91f396979d0b3c0493dc
yugestrive <15090221166@163.com>
2020-03-16 15:17:32 +0800

    update

--- src/service/interceptor.js
+++ src/service/interceptor.js
@@ -7,12 +7,7 @@ import wxApiInterceptors from 'wxapp-api-interceptors'
 // 引入Auth
 import Auth from '../utils/auth'
 // 引入全局URL
-import { GLOBAL_URL } from './config'
-
-// 定义request头部参数
-const OPTIONS = {
-    headers: { 'Content-Type': 'application/json;charset=utf-8' }
-}
+import { GLOBAL_URL } from '../config'
 
 // 对wx.request的拦截、还可对其他wx的API进行拦截设置
 wxApiInterceptors({
@@ -26,8 +21,8 @@ wxApiInterceptors({
             const reg = /^(http|https)?:\/\//
             if (!reg.test(request.url)) {
                 request.url = GLOBAL_URL + request.url
-                
-            } else if(token && !request.headers.token) {
+            }
+            if(token && !request.headers.token) {
                 request.headers.token = `${token}`
             }
             console.log('request.url:', request.url, 'header:', request.headers)
@@ -47,30 +42,3 @@ wxApiInterceptors({
         }
     }
 })
-
-// 定义wx请求类
-export class WXRequest {
-    /**
-     * @param {Object} options
-     * @param {string} options.url 请求路径
-     * @param {string} options.method 请求路径
-     * @param {Object} [options.data = {}] 请求数据
-     * @param {Object} [options.headers = {}] 请求头
-     */
-    constructor({ url, method = 'POST', data = {}, headers = {} } = {}) {
-        if(!url) {
-            throw new RangeError('缺少 url 参数')
-        }
-        
-        this.method = method
-        // 如果没有传入 headers 使用 OPTIONS 对应当前环境的 headers 配置
-        this.headers = headers || OPTIONS.headers
-        console.log(this.headers, 'this.header')
-        if (this.method === 'GET') {
-            this.url = url + getParams(data)
-        } else {
-            this.url = url
-            this.data = data
-        }
-    }
-}
--- src/service/request.js
+++ src/service/request.js
@@ -1,9 +1,6 @@
-// 引入全局URL
-import { GLOBAL_URL } from './config'
-
 // 定义request头部参数
 const OPTIONS = {
-    header: { 'Content-Type': 'application/json;charset=utf-8' }
+    headers: { 'Content-Type': 'application/json;charset=utf-8' }
 }
 
 const getParams = (data) => {
@@ -23,15 +20,30 @@ const getParams = (data) => {
 
 // 定义wx请求类
 export class WXRequest {
-    constructor({url = '', method = 'POST', header = {}, data = {}} = {}) {
-        // URL匹配http若传有带http/https/协议的
-        let reg = /^(http|https)?:\/\//
+    /**
+     * @param {Object} options
+     * @param {string} options.url 请求路径
+     * @param {string} options.method 请求路径
+     * @param {Object} [options.data = {}] 请求数据
+     * @param {Object} [options.headers = {}] 请求头
+     */
+    constructor({ url, method = 'POST', data = {}, headers = {} } = {}) {
+        if(!url) {
+            throw new RangeError('缺少 url 参数')
+        }
+        
         this.method = method
-        this.header = header | OPTIONS.header
-        this.url = (reg.test(url) ? url : (GLOBAL_URL + url)) + (this.method === 'GET' ? getParams(data) : '')
-        this.data = this.method === 'GET' ? {} : data
+        // 如果没有传入 headers 使用 OPTIONS 对应当前环境的 headers 配置
+        this.headers = headers || OPTIONS.headers
+        console.log(this.headers, 'this.header')
+        if (this.method === 'GET') {
+            this.url = url + getParams(data)
+        } else {
+            this.url = url
+            this.data = data
+        }
     }
-    q() {
+    send() {
         return new Promise((resolve, reject) => {
             wx.request({
                 url: this.url,
@@ -39,10 +51,7 @@ export class WXRequest {
                 header: this.header,
                 data: this.data,
                 success: res => {
-                    // 定义拦截
-                    if (res.statusCode === 200) {
-                        resolve(res.data)
-                    }
+                    resolve(res.data)
                 },
                 fail: res => {
                     reject(res.errMsg)
